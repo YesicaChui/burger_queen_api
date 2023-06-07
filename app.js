b3ebad4d2e9ab8494614c9e4e7da1608acd2dc3a
YesicaChui <yesicachuic@gmail.com>
2023-06-06 23:28:09 -0500

    rutas de productos y ordenes con base de datos mockeada datos no persistentes

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post('/users', (req, res) => {
     "error": "string"
   });
   //crear el id --> buscaremos el mayor id de usuarios y le aumentaremos 1
-  const idMayor = db.users.reduce((acumulador, elemento) => acumulador < elemento.id ? elemento.id : acumulador, 1)
+  const idMayor = db.users.reduce((acumulador, elemento) => acumulador < elemento.id ? elemento.id : acumulador, 0)
   // creo el objeto nuevoUsuario para insertar al arreglo de objetos
   const nuevoUsuario = {
     "email": req.body.email,
@@ -122,6 +122,160 @@ app.delete('/users/:uid', (req, res) => {
     });
     db.users.splice(indiceUsuario,1)
   res.send("usuaria eliminada")
+})
+
+
+//Rutas products Operaciones sobre productos
+app.get('/products', (req, res) => {
+  // falta validar que envien un token valido
+  console.log("Lista productos")
+  res.send(db.products)
+})
+
+app.post('/products', (req, res) => {
+  console.log("Crear producto")
+  // si no hay name o no hay price retorna 400 y el objeto error
+  if (!req.body.name || !req.body.price) return res.status(400).send({
+    "error": "string"
+  });
+
+  //crear el id --> buscaremos el mayor id de productos y le aumentaremos 1
+  const idMayor = db.products.reduce((acumulador, elemento) => acumulador < elemento.id ? elemento.id : acumulador, 0)
+  // creo el objeto nuevoProducto para insertar al arreglo de objetos
+  const nuevoProducto = {
+    "name":  req.body.name,
+    "price": req.body.price,
+    "image": req.body.image,
+    "type": req.body.type,
+    "id": idMayor+1
+  }
+  // inserto al nuevoProducto
+  db.products.push(nuevoProducto)
+  // respondo al nuevo producto
+  res.send(nuevoProducto)
+})
+
+app.get('/products/:uid', (req, res) => {
+  console.log("Obtiene informacion de un producto")
+  // obtenemos el id del url Request
+  const id = req.params.uid
+  // traigo el elemento que coincida el id con el id del arreglo de objetos de productos 
+  const product = db.products.find(elemento => elemento.id == id)
+  // si no lo encuentra envia mensaje de error
+  if (!product) return res.status(404).send({
+    "error": "string"
+  });
+  res.send(product)
+})
+app.patch('/products/:uid', (req, res) => {
+  console.log("Modifica un producto")
+    // obtenemos el id del url Request
+    const id = req.params.uid
+    // traigo el elemento que coincida el id con el id del arreglo de objetos de productos 
+    const indiceProducto = db.products.findIndex(elemento => elemento.id == id)
+    // si no lo encuentra envia mensaje de error
+    if (indiceProducto==-1) return res.status(404).send({
+      "error": "string"
+    });
+    db.products[indiceProducto]={
+      ...db.products[indiceProducto],
+      ...req.body
+    }   
+
+  res.send("Modifica un producto")
+})
+
+app.delete('/products/:uid', (req, res) => {
+    // obtenemos el id del url Request
+    const id = req.params.uid
+    // traigo el elemento que coincida el id con el id del arreglo de objetos de productos 
+    const indiceProducto = db.products.findIndex(elemento => elemento.id == id)
+    // si no lo encuentra envia mensaje de error
+    if (indiceProducto==-1) return res.status(404).send({
+      "error": "string"
+    });
+    // uso Splice para quitar un producto
+    db.products.splice(indiceProducto,1)
+  res.send("producto eliminado")
+})
+
+//Rutas orders Operaciones sobre ordenes
+
+app.get('/orders', (req, res) => {
+  // falta validar que envien un token valido
+  console.log("Lista ordenes")
+  res.send(db.orders)
+})
+
+app.post('/orders', (req, res) => {
+  console.log("Crear order")
+  // si no hay name o no hay price retorna 400 y el objeto error
+  if (!req.body.products || req.body.products.length==0) return res.status(400).send({
+    "error": "string"
+  });
+  //crear el id --> buscaremos el mayor id de Orders y le aumentaremos 1
+  const idMayor = db.orders.reduce((acumulador, elemento) => acumulador < elemento.id ? elemento.id : acumulador, 0)
+  // creo el objeto nuevaOrder para insertar al arreglo de objetos
+  const nuevaOrder = {
+    "id": idMayor+1,
+    "client": req.body.client,
+    "products": req.body.products,
+    "status": req.body.status,
+    "dateEntry": req.body.dateEntry
+  }
+  
+  // inserto la nuevaOrder
+  db.orders.push(nuevaOrder)
+  // respondo al nueva Order
+  res.send(nuevaOrder)
+})
+
+app.get('/orders/:uid', (req, res) => {
+  console.log("Obtiene informacion de una Orden")
+  // obtenemos el id del url Request
+  const id = req.params.uid
+  // traigo el elemento que coincida el id con el id del arreglo de objetos de ordenes 
+  const order = db.orders.find(elemento => elemento.id == id)
+  // si no lo encuentra envia mensaje de error
+  if (!order) return res.status(404).send({
+    "error": "string"
+  });
+  res.send(order)
+})
+app.patch('/orders/:uid', (req, res) => {
+  console.log("Modifica una orden")
+  // si no hay status nos da error de mal hecha la petición
+  if (!req.body.status ) return res.status(400).send({
+    "error": "string"
+  });
+    // obtenemos el id del url Request
+    const id = req.params.uid
+    // traigo el elemento que coincida el id con el id del arreglo de objetos de ordenes 
+    const indiceOrden = db.orders.findIndex(elemento => elemento.id == id)
+    // si no lo encuentra envia mensaje de error
+    if (indiceOrden==-1) return res.status(404).send({
+      "error": "string"
+    });
+    db.orders[indiceOrden]={
+      ...db.orders[indiceOrden],
+      ...req.body
+    }   
+
+  res.send("Modifica una orden")
+})
+
+app.delete('/orders/:uid', (req, res) => {
+    // obtenemos el id del url orders
+    const id = req.params.uid
+    // traigo el elemento que coincida el id con el id del arreglo de objetos de orders 
+    const indiceOrder = db.orders.findIndex(elemento => elemento.id == id)
+    // si no lo encuentra envia mensaje de error
+    if (indiceOrder==-1) return res.status(404).send({
+      "error": "string"
+    });
+    // uso Splice para quitar un Orden
+    db.orders.splice(indiceOrder,1)
+  res.send("Orden eliminada")
 })
 
 // inicio el servidor en el puerto 3000
