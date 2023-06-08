feb8ad692772b90d2aa556e7ab31bedb2c5b08d3
YesicaChui <yesicachuic@gmail.com>
2023-06-08 04:19:17 -0500

    agregando script npm start

--- src/routers/login.route.js
+++ src/routers/login.route.js
@@ -1,5 +1,4 @@
 import {Router} from 'express'
-import { db } from '../data/db.js'
 import { generateToken } from '../utils/utils.js'
 import userModel from '../models/user.model.js'
 import { isValidPassword } from '../utils/utils.js'
@@ -14,12 +13,6 @@ router.post('/', async (req, res) => {
   if (!email || !password) return res.status(400).send({
     "error": "string"
   });
-/*   // busco en la BD si hay usuario que coincida su  email y password con el del request
-  const validarUsuario = db.users.findIndex((elemento) => elemento.email == req.body.email && elemento.password == req.body.password)
-  // si no lo encuentra retorno error
-  if (validarUsuario == -1) return res.status(404).send({
-    "error": "string"
-  }); */  
 
   // traigo el elemento que coincida el email con el id de MongoDB
   const user = await userModel.findOne({ email }).lean().exec()
@@ -28,6 +21,8 @@ router.post('/', async (req, res) => {
     "error": "string"
   });
 
+  // valido si la contraseña ingresada es valida comparandola con el de la BD
+  // usando bcrypt para compararla con la contraseña encriptada
   if(!isValidPassword(user, password)) return res.status(404).send({
     "error": "string"
   });
