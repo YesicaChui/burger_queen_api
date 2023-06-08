91eb25fd3655a43342250f1d0cfd9d6e2f51e5ad
YesicaChui <yesicachuic@gmail.com>
2023-06-08 00:45:06 -0500

    actualizando el usuario de mongoDB y borrando el usuario de mongoDB

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -54,8 +54,8 @@ router.get('/:uid', async (req, res) => {
   // obtenemos el id del url Request
   const id = req.params.uid
   // traigo el elemento que coincida el id con el id del arreglo de objetos de usuario 
- // const user = db.users.find(elemento => elemento.id == id)
- const user = await userModel.findOne({ id }).lean().exec()
+  // const user = db.users.find(elemento => elemento.id == id)
+  const user = await userModel.findOne({ id }).lean().exec()
   // si no lo encuentra envia mensaje de error
   if (!user) return res.status(404).send({
     "error": "string"
@@ -63,35 +63,68 @@ router.get('/:uid', async (req, res) => {
   res.send(user)
 })
 
-router.patch('/:uid', (req, res) => {
+router.patch('/:uid', async (req, res) => {
   console.log("Modifica una usuaria")
   // obtenemos el id del url Request
   const id = req.params.uid
-  // traigo el elemento que coincida el id con el id del arreglo de objetos de usuario 
-  const indiceUsuario = db.users.findIndex(elemento => elemento.id == id)
-  // si no lo encuentra envia mensaje de error
-  if (indiceUsuario == -1) return res.status(404).send({
-    "error": "string"
-  });
-  db.users[indiceUsuario] = {
-    ...db.users[indiceUsuario],
-    ...req.body
+  /*   // traigo el elemento que coincida el id con el id del arreglo de objetos de usuario 
+    const indiceUsuario = db.users.findIndex(elemento => elemento.id == id)
+    // si no lo encuentra envia mensaje de error
+    if (indiceUsuario == -1) return res.status(404).send({
+      "error": "string"
+    });
+    db.users[indiceUsuario] = {
+      ...db.users[indiceUsuario],
+      ...req.body
+    } */
+  try {
+    const result = await userModel.updateOne({ id }, { ...req.body })
+    // matchedCount = coincidencia encontrada
+    console.log(result.matchedCount)
+    // si no existe coincidencia con el id retorna 404
+    if (!result.matchedCount) {
+      return res.status(404).send({
+        error: 'string'
+      });
+    }
+    res.send({ ...req.body })
+  } catch (err) {
+    console.log(`error insertando: ${err}`)
+    // retorno 500 si hay otro problema talvez con el mongoDB
+    res.status(500).send({
+      "error": "string"
+    });
   }
 
-  res.send("Modifica una usuaria")
+
 })
 
-router.delete('/:uid', (req, res) => {
+router.delete('/:uid', async (req, res) => {
   // obtenemos el id del url Request
   const id = req.params.uid
-  // traigo el elemento que coincida el id con el id del arreglo de objetos de usuario 
-  const indiceUsuario = db.users.findIndex(elemento => elemento.id == id)
-  // si no lo encuentra envia mensaje de error
-  if (indiceUsuario == -1) return res.status(404).send({
-    "error": "string"
-  });
-  db.users.splice(indiceUsuario, 1)
-  res.send("usuaria eliminada")
+  /*   // traigo el elemento que coincida el id con el id del arreglo de objetos de usuario 
+    const indiceUsuario = db.users.findIndex(elemento => elemento.id == id)
+    // si no lo encuentra envia mensaje de error
+    if (indiceUsuario == -1) return res.status(404).send({
+      "error": "string"
+    });
+    db.users.splice(indiceUsuario, 1) */
+  try {
+    // eliminando usuario por id
+    const respuesta = await userModel.deleteOne({ id })
+    console.log(respuesta)
+    //deleteCount = conteo de borrados
+    // si el conteo de borrados es 0 entonces retorno 404 no encontrado
+    if (!respuesta.deletedCount) return res.status(404).send({
+      "error": "string"
+    });
+    res.send("usuaria eliminada")
+  } catch (err) {
+    console.log(`error: ${err}`)
+    return res.status(500).send({
+      "error": "string"
+    });
+  }
 })
 
 export default router
